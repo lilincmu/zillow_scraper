c8c6e8a8b581c4381d0111e13c94082ef2d63b69
lilincmu <lilincmu@gmail.com>
2020-11-11 13:29:34 -0800

    Add more sections

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # zillow_scraper
+Simple scraper to get text value of zillow listings
--- zillow.py
+++ zillow.py
@@ -11,11 +11,14 @@ def generate_map_from_list(text_list):
 	text_map = {}
 	i = 0
 	while i < len(text_list):
-		if text_list[i].find(': ') < 0:
+		colon_pos = text_list[i].find(':')
+		if colon_pos < 0:
 			i += 1
 			continue
 
-		text_map[text_list[i][:len(text_list[i]) - 2]] = text_list[i + 1]
+		if text_list[i][:colon_pos] in text_map:
+			print("Overwritten attribute ", text_list[i][:colon_pos])
+		text_map[text_list[i][:colon_pos]] = text_list[i + 1]
 		i += 2
 	return text_map
 
@@ -35,20 +38,29 @@ headers= {
 response = requests.get(args.url,headers=headers,cookies=cj)
 document = html.fromstring(response.text)
 
+# summary section
+summary = document.xpath("//div[@class='ds-summary-row']")[0]
+price = summary.xpath(".//span[@class='ds-value']//text()")[0]
+area = summary.xpath(".//span[@class='ds-bed-bath-living-area']//text()")[6]
+# print(price, area)
+
+# facts and feature section
 facts_and_features = document.xpath("//div[contains(@class, 'ds-home-facts-and-features')]")[0]
-facts_list = facts_and_features.xpath("./div/div[1]//ul")[0]
-second_span_format = './li[{num}]//span[2]//text()'
-house_type = facts_list.xpath(second_span_format.format(num=1))[0]
-year_built = facts_list.xpath(second_span_format.format(num=2))[0]
-heating = facts_list.xpath(second_span_format.format(num=3))[0]
-cooling = facts_list.xpath(second_span_format.format(num=4))[0]
-parking = facts_list.xpath(second_span_format.format(num=5))[0]
-hoa_fee = facts_list.xpath(second_span_format.format(num=6))[0]
-lot_size = facts_list.xpath(second_span_format.format(num=7))[0]
+facts_list = facts_and_features.xpath("./div/div[1]//text()")
+# print(generate_map_from_list(facts_list))
 
-# print(house_type, year_built, heating, cooling, parking, hoa_fee, lot_size)
+details_section = facts_and_features.xpath("./div/div[2]//text()")
+# print(generate_map_from_list(details_section))
 
-details = facts_and_features.xpath("./div/div[2]//text()")
-# print(generate_map_from_list(details))
-
+# school score section
+school_list = document.xpath("//ul[@class='ds-nearby-schools-list']")[0]
+school_score_format = './li[{num}]//span[1]//text()'
+school_name_format = './li[{num}]//a//text()'
+schools = []
+for i in [1, 2, 3]:
+	schools.append({
+		'score': school_list.xpath(school_score_format.format(num=i))[0],
+		'name': school_list.xpath(school_name_format.format(num=i))[0],
+	})
+# print(schools)
 
